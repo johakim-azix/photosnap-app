0082b2f8ccc8e789b809f0b8be427b1dc2577d73
penda joseph <johakimazix4@gmail.com>
2022-05-28 02:52:14 +0200

    reviewed stories and features pages, started pricing page : completed the pricing grid

---
+++ src/components/pricing/pricing-grid.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "pricing-grid"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/pricing/pricing-tab.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "pricing-tab"
+    }
+</script>
+
+<style scoped>
+
+</style>
